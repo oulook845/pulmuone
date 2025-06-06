01547544007edc4b14575bfc75c55d6c392bbaef
jiho <audwlgh0221@google.com>
2025-06-07 07:36:20 +0900

    반응형 작업

--- js/index.js
+++ js/index.js
@@ -1,5 +1,8 @@
 import { getScrollData } from "./commonScroll.js";
 
+/* Media Query */
+const bp_tablet = 958;
+const bp_mobile = 596;
 
 /* dom 요소 */
 const headerElem = document.getElementById("header"),
@@ -21,27 +24,61 @@ function headerFuntion(active) {
     headerElem.classList.add("active");
   }
 }
-// header에 active 클래스가 있을 때, 
+// header에 active 클래스가 있을 때,
 header_SubMenu.forEach((SubMenu) => {
   SubMenu.addEventListener("mouseenter", function () {
-    if(headerElem.classList.contains("active")){
+    if (headerElem.classList.contains("active")) {
       header_NavBg.style.height = "90px";
-    }else return
+    } else return;
   });
   SubMenu.addEventListener("mouseleave", function () {
-    if(headerElem.classList.contains("active")){
+    if (headerElem.classList.contains("active")) {
       header_NavBg.style.height = "0px";
-    }else return
+    } else return;
   });
 });
 
+/* visual 스크립트 ################## */
+const visual_slide = visualElem.querySelector("ul.bgSlide"),
+  visual_slideList = visual_slide.querySelectorAll("li");
+
+const visual_slideLength = visual_slideList.length - 1;
+let visual_idx = 0;
+let visual_intervalId;
+
+function m_visualAni() {
+  if (visual_idx < visual_slideLength) {
+    visual_idx++;
+  } else {
+    visual_idx = 0;
+  }
+  visual_slideList.forEach((list) => {
+    list.style.opacity = 0;
+  });
+  visual_slideList[visual_idx].style.opacity = 1;
+}
+function m_visualSlide() {
+  let currentMedia = window.outerWidth;
+  if (currentMedia <= bp_tablet) {
+    visual_intervalId = setInterval(() => {
+      m_visualAni();
+    }, 4000);
+  } else {
+    clearInterval(visual_intervalId);
+  }
+}
+m_visualSlide();
+
 /* con2 스크립트 ################## */
 const con2_imgList = con2Elem.querySelector(".img_area");
-const con2_imgList_Height = con2_imgList.getBoundingClientRect().height;
+let con2_imgList_Height = con2_imgList.getBoundingClientRect().height;
 
-/* sticky를 위해 #con2높이 = 전체 이미지 높이 */
-con2Elem.style.height = `${con2_imgList_Height}px`;
-
+function con2_slideHeight() {
+  con2_imgList_Height = con2_imgList.getBoundingClientRect().height;
+  /* sticky를 위해 #con2높이 = 전체 이미지 높이 */
+  con2Elem.style.height = `${con2_imgList_Height}px`;
+}
+con2_slideHeight();
 
 /* 스크롤 이벤트시 필요한 콘텐츠별 Y축 위치 */
 const visualTop = visualElem.offsetTop,
@@ -71,8 +108,7 @@ function currentScroll() {
     } else if (currentScrollY > con2Top && currentScrollY <= con3Top) {
       /* con2 영역 시작 */
       // console.log("con2 도착");
-    }
-     else if (currentScrollY > con3Top && currentScrollY <= con4Top) {
+    } else if (currentScrollY > con3Top && currentScrollY <= con4Top) {
       /* con3 영역 시작 */
       // console.log("con3 도착");
     } else if (currentScrollY > con4Top && currentScrollY <= footerTop) {
@@ -84,20 +120,33 @@ function currentScroll() {
 currentScroll();
 
 /* con3 슬라이드 이벤트 */
-function con3Slide(){
+function con3Slide() {
   // 변화된 요소를 업데이트 해야 해서 함수 내부에 선언
   const con3SlideElem = con3Elem.querySelector(".slide_list");
   const con3SlideLists = con3SlideElem.querySelectorAll("li");
   const firstList = con3SlideLists[0].cloneNode(true);
-  con3SlideElem.appendChild(firstList)
-  con3SlideElem.style.transition = "1s";
-  con3SlideElem.style.marginLeft = "-542px";
-  setTimeout(function(){
+  let con3SlideWidth = con3SlideLists[0].offsetWidth;
+  console.log(con3SlideWidth);
+
+  con3SlideElem.appendChild(firstList);
+  con3SlideElem.style.transition = "margin-left 1s";
+  con3SlideElem.style.marginLeft = `-${con3SlideWidth}px`;
+  setTimeout(function () {
     con3SlideLists[0].remove();
     con3SlideElem.style.transition = "0s";
     con3SlideElem.style.marginLeft = "0px";
-  },1000);
+  }, 1000);
 }
-setInterval(con3Slide, 4000); //첫번째 슬라이드를 복사후 없애고 전체 슬라이드 마지막에 만들기
+let con3_intervalId = setInterval(con3Slide, 4000); //첫번째 슬라이드를 복사후 없애고 전체 슬라이드 마지막에 만들기
 
-
+/* 미디어 크기 변경시 이벤트 */
+window.addEventListener("resize", function () {
+  /* visual */
+  clearInterval(visual_intervalId);
+  m_visualSlide();
+  /* con2 */
+  con2_slideHeight();
+  /* con3 */
+  clearInterval(con3_intervalId);
+  con3_intervalId = setInterval(con3Slide, 4000);
+});
